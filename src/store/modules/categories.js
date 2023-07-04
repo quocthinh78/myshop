252d75723cd2e77e47df53d25d92c864f48d6e32
quocthinh78 <thinhvq@axpara.com>
2023-07-04 14:02:48 +0700

    merge main

--- src/store/modules/categories.js
+++ src/store/modules/categories.js
@@ -1,2 +1,7 @@
 const categories = [{ name: "thinh2h", id: 1 }];
-// update on stssaging1
+// update 3
+// update stash 2
+// update stash 1
+// add stash 1
+// add tash 2
+// code mowis cuar stasging ne
